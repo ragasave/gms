940c1f6d111cfe23a43ae241e8ac792ded3f7c3f
Rahul Varma <ragasave@gmail.com>
2020-12-19 12:55:27 +0530

    updated

--- head.php
+++ head.php
@@ -5,3 +5,5 @@ include_once './config.php';
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?= $config['seo_home_title'] ?></title>
 <link rel="stylesheet" href="css/styles.css">
+
+<meta name="google-site-verification" content="RqC0Dc6mjkce9GIAECEL8shMX_p6dud0sWAlcjjAk7Y" />
