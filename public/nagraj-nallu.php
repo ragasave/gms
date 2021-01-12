87991d7b340a5d6fb723d9c860c2b7b1cb950a18
Rahul Varma <ragasave@gmail.com>
2021-01-12 09:52:16 +0530

    updated

--- public/nagraj-nallu.php
+++ public/nagraj-nallu.php
@@ -40,9 +40,19 @@
                 <hr>
                 <br>
                 <p>
-                <span class="ttc">Mr. Nagraj Nallu</span> (MBA in Marketing from IBM, Mumbai) a successful entrepreneur and highly experienced marketing professional is the Founder and Chief Executive Officer (CEO) of NewAge Realty. With a demonstrated history of working in consumer durable sector, banking and real estate industry, he started NewAge Realty in 2012 with an agenda of changing the state of affairs of Real Estate industry in India. 
-                    <br>
-                    Mr. Nallu innovated the Real Estate selling approach via call centres while working with Times of India. Skilled in Negotiation, Business Planning, Sales, Customer Relationship Management (CRM) and Team Building he is also one of the pioneers to sell Indian properties in International market.
+                <span class="ttc">Mr. Nagraj Nallu</span> (MBA in Marketing from IBM), a successful Entrepreneur and highly experienced marketing expert started his career in the banking industry. Mr. Nallu worked as Branch Manager with DCB in 2006 where he took care of day to day banking operations, managing sales, profit & loss of the branch. He was later promoted to Regional Sales Manager in 2008.
+                <br>
+                <br>
+He headed the real estate segment at Artha Money (Times of India Group) and pioneered to setup the Call center & established direct sales channel for the organization. Here’s when he got a flair of real estate sector and then there was no looking back.
+                <br>
+                <br>
+In 2013, Mr. Nallu was the Managing partner at NewAge Realty & Commercial Brokerage LLC – Dubai. With his innovative ideas, he immerged as a pathbreaking real estate marketer and promoter. Instead of selling properties as a usual real estate agent, Mr. Nallu focused on Brand Development, Web Traffic growth, Implementing Strategy & Statistical Marketing Mix and Sales Strategy development for forerunners in real estate sector. He was one of the few Realtors who had opened gates for establishing new marketing avenues for Indian Realty companies in GCC region. It was under his guidance and leadership that NewAge Realty & Commercial Brokerage LLC – Dubai was the sole seller of properties of India Bulls outside India.
+                <br>
+                <br>
+Mr. Nallu then established his company in India by the name of NewAge Realfin Pvt. Ltd. where he generated new expansion deals for a real estate development company that focuses on multi-family residential projects through his real estate market analysis. He also developed the company to become global real-estate marketing company through collaborations with international property developers. Mr. Nallu is now an established Indian master channel partner for the companies from Cyprus, Singapore, UK and UAE managing sales for Indian Real estate developers through his associates in rest of the world where Indian population resides.
+                <br>
+                <br>
+With his gigantic experience of over 15 years in Banking, finance & Real Estate sector, we are extremely delighted to have Mr. Nagraj Nallu as our CEO at Glocal Marketing Solutions Pvt. Ltd. With his skills and ability to enter the world market effortlessly, we look forward to take his experience and encourage Indian Companies, Brands, Products to reach out worldwide and support - Make in India (An initiative by Shri Narendra Modi, Prime Minister of India)
                 </p>
             </div>
         </div>
