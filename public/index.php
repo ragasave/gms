3394bfc49da631f6b2c69e3f17251a68786d73b2
Rahul Varma <ragasave@gmail.com>
2021-02-22 15:36:15 +0530

    lionel-velloz.php added

--- public/index.php
+++ public/index.php
@@ -249,6 +249,27 @@
                             </a>
                         </div>
                     </div>
+                    <div>
+                        <div class="solution-section-s1-item">
+                            <a href="lionel-velloz.php">
+                                <div class="solution-section-s1-item-wpr">
+                                    <div class="solution-section-s1-item-name">
+                                        <h3 class="solution-section-s1-item-name-txt tc">Mr. Lionel Vollez</h3>
+                                    </div>
+                                    <div class="solution-section-s1-item-imgw solution-section-s1-item-imgw-s">
+                                        <div class="solution-section-s1-item-img" style="background-image:url(images/lionel-velloz.jpeg)"></div>
+                                    </div>
+                                    <div class="solution-section-s1-item-desc">
+                                        <p></p>
+                                        <!-- <p>
+                                        Mr. Nagraj Nallu (MBA in Marketing from IBM, Mumbai) a successful entrepreneur and highly experienced marketing professional is the Founder and Chief Executive Officer (CEO) of NewAge Realty. With a demonstrated history of working in consumer durable sector, banking and real estate industry, he started NewAge Realty in 2012 with an agenda of changing the state of affairs of Real Estate industry in India. Mr. Nallu innovated the Real Estate selling approach via call centres while working with Times of India. Skilled in Negotiation, Business Planning, Sales, Customer Relationship Management (CRM) and Team Building he is also one of the pioneers to sell Indian properties in International market.
+                                        </p> -->
+                                        <!-- <a href="" class="solution-section-s1-item-details-link">More</a> -->
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ public/lionel-velloz.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once('./../head.php'); ?>
+</head>
+
+<body>
+    <?php include_once('./../header.php'); ?>
+
+
+    <div class="top-banner">
+        <div class="top-banner-img" style="background-image: url('./images/876543.png   ');">
+
+        </div>
+    </div>
+
+
+    <!-- <section class=" section page-navigate-section">
+        <div class="section-container">
+            <br>
+            <span class="nvgt-home">Home </span> <span class="nvgt-home">Our Teams</span> <span class="nvgt-current">Mr. Nagraj Nallu</span>
+            <br>
+            <br>
+        </div>
+    </section> -->
+
+    <section class="section xx--section">
+        <br>
+        <br>
+        <div class="section-container lh">
+            <div class="xx-out-teams">
+                <div class="xx-out-teams-img">
+                    <img src="images/lionel-velloz.jpeg" alt="" srcset="">
+                </div>
+                <br>
+                <!-- <h4 class="tc">Founder</h4> -->
+                <br>
+                <br>
+                <hr>
+                <br>
+                <p>
+                Having over 20 years of business & management experience, we are extremely delighted to have Mr. Lionel Velloz as one of our Directors at Glocal Marketing Solutions Pvt. Ltd. He holds a Bachelors of Commerce degree from Mumbai University (2001) and Masters in Computer Science from Bapna Computers Limited, Mumbai.
+                <br>
+                <br>
+                Mr. Lionel Vollez started his career Ace Technologies as Senior Executive. It was then when he started grasping the skills of HR functions, recruiting and training employees, administering compensation & benefits, grievance handling, employee engagement and people management. His role of interest did not limit to HR activities, he quickly involved himself in business development, credit control and quality checks & verifications.
+                <br>
+                <br>
+                Mr. Lionel Velloz is currently the Director at Ace Technologies & Infrastructure Pvt. Ltd. Ace Technologies & Infrastructure Pvt. Ltd. is a rapidly growing company promoted by group of technocrats with experiences over 12 years in the space of security system, building automation and infrastructure. 
+                <br>
+                <br>
+                Mr. Velloz is also the Chairman of a listed company by the name of Art of Technologies Infrastructure Limited which was incorporated in 1996 at Mumbai. This company is a diversified, value-focused and asset-based company that undertakes wealth-based projects specializing in Infrastructure and Info-Tech. AoT’ claims to cover global gambit of technology solutions – an “IT supermall” and a “e-Realty supermall”
+                <br>
+                <br>
+                With such enormous experience in business management, training and development, we are glad to have him on board and are looking forward to learning more about successful business practice from Mr. Lionel Velloz.
+                </p>
+
+            </div>
+        </div>
+    </section>
+
+    <br>
+    <br>
+    <br>
+    <br>
+
+
+    <style>
+        .xx--section{
+            /* background-color: #fff; */
+            text-align: justify;
+        }
+        .xx-out-teams-img{
+            width: 200px;
+            height: 200px;
+            text-align: center;
+            margin: auto;
+            border-radius: 50%;
+            border: 2px solid #0000003d;
+            overflow: hidden;
+
+        }
+        .xx-out-teams-img img{
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+
+    </style>
+    <?php include_once('./../footer.php'); ?>
+    <?php include_once('./../script.php'); ?>
+</body>
+
+</html>
